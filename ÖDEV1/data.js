f5f5e3f220e9f107fafff4757ba471fad355377a
Kağan Demiröz <44818302+SgtKOCD@users.noreply.github.com>
2022-02-05 22:07:20 +0300

    Update data.js

--- ÖDEV1/data.js
+++ ÖDEV1/data.js
@@ -2,8 +2,8 @@ function odev1 () {
     console.log(this.responseText);
   }
   
-  var nesneCek = new XMLHttpRequest();
-  var url="https://jsonplaceholder.typicode.com/users"
-  nesneCek.addEventListener("load", odev1);
-  nesneCek.open("GET", url);
+  var nesneCek = new XMLHttpRequest(); //child object
+  var url="https://jsonplaceholder.typicode.com/users" // url 
+  nesneCek.addEventListener("load", odev1);//listener
+  nesneCek.open("GET", url);//open the data
   nesneCek.send();
